dac930676e3fcbbf815e99c575d649b666e1237c
André Breda <addobandre99@gmail.com>
2015-12-15 23:11:29 +0000

    Travis CI: Maybe you'll work this time?\nAlso, there are apparently no native modules here

--- travis.sh
+++ travis.sh
@@ -17,13 +17,17 @@ git init
 git config user.name "Travis CI"
 git config user.email "the-mighty-travis-bot@example.org"
 
+# Make sure there is a .nojekyll file here
+touch .nojekyll
+
 # The first and only commit to this new Git repo contains all the
 # files present with the commit message "Deploy to GitHub Pages".
 git add .
+git add .nojekyll # Almost sure it wouldn't be added otherwise
 git commit -m "Deploy to GitHub Pages"
 
 # Force push from the current repo's master branch to the remote
 # repo's master branch. (All previous history on the master branch
 # will be lost, since we are overwriting it.) We redirect any output to
 # /dev/null to hide any sensitive credential data that might otherwise be exposed.
-git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:master > /dev/null 2>&1
+git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" > /dev/null 2>&1
